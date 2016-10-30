357f0635f6037194bf9036ead9021f4539b155a8
gdmec07150628 <254438331@qq.com>
2016-10-30 17:59:32 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150628/intent/IntentDemo3.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.s07150628.intent;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class IntentDemo3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+       }
